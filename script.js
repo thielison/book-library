2fa020785a50728ea039d1c329f0f0842ada0131
Thielison Alves <67246254+thielison@users.noreply.github.com>
2023-07-12 20:03:41 -0300

    Update functions
    - Update addBookToLibrary function to pass the index of the book in the
    myLibrary array as an argument to the addNewBookCardDiv function
    - Update addNewBookCardDiv function to set the data-index attribute of
    the new book card to its index in the myLibrary array
    - Add functionality to removeBookFromLibrary function to remove a book
    from the myLibrary array and its corresponding book card from the DOM
    - Update the data-index attribute of all remaining book cards in
    removeBookFromLibrary function to reflect their new position in the library

--- script.js
+++ script.js
@@ -16,12 +16,13 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 
     let lastBookAddedToMyLibrary = myLibrary.slice(-1);
-    addNewBookCardDiv(lastBookAddedToMyLibrary[0]);
+    addNewBookCardDiv(lastBookAddedToMyLibrary[0], myLibrary.length - 1);
 }
 
-function addNewBookCardDiv(book) {
+function addNewBookCardDiv(book, index) {
     const newDiv = document.createElement("div");
-    newDiv.classList = `book #${myLibrary.length}`;
+    newDiv.classList = `book`;
+    newDiv.setAttribute("data-index", index);
 
     const titlePara = document.createElement("p");
     titlePara.textContent = `"${book.title}"`;
@@ -61,8 +62,18 @@ function changeReadStatus(e) {
     e.classList = `read-status ${isRead ? "not-read" : "read"}`;
 }
 
-function removeBookFromLibrary(e) {
-    e.parentElement.remove();
+function removeBookFromLibrary(target) {
+    // Removes a book from the myLibrary array and its corresponding book card from the DOM.
+    let bookCard = target.closest(".book");
+    let index = bookCard.getAttribute("data-index");
+    myLibrary.splice(index, 1);
+    bookCard.remove();
+
+    // Updates the data-index attribute of all remaining book cards to reflect their new position in the library.
+    let bookCards = document.querySelectorAll(".book");
+    for (let i = 0; i < bookCards.length; i++) {
+        bookCards[i].setAttribute("data-index", i);
+    }
 }
 
 function getFormData(form) {
@@ -105,7 +116,8 @@ addBookButton.addEventListener("click", displayForm);
 
 document.addEventListener("click", handleClickOutsideForm);
 
-const testBook = new Book("The Hobbit", "J. R. R. Tolkien", 304, null);
-const testBook2 = new Book("The Book Thief", "Markus Zusak", 552, "on");
-addBookToLibrary(testBook);
-addBookToLibrary(testBook2);
+// Test books that are displayed when the page loads
+const book1 = new Book("The Hobbit", "J. R. R. Tolkien", 304, null);
+addBookToLibrary(book1);
+const book2 = new Book("The Book Thief", "Markus Zusak", 552, "on");
+addBookToLibrary(book2);
